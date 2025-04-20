7460803134587e717f4ce9e436b694534f254bb8
astanea <adrianstanea1@gmail.com>
2025-04-20 17:51:13 +0300

    Implement core logic for JS acion
    Signed-off-by: astanea <adrianstanea1@gmail.com>

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,18 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('child_process')
+const core = require("@actions/core");
+// const github = require("@actions/github");
+const exec = require("child_process");
 
 function run() {
-    core.notice('Hello from deploy-s3-javascript action!')
+    // TODO:
+    //  1. Get input values
+    const bucket = core.getInput("bucket", { required: true, trimWhitespace: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true, trimWhitespace: true });
+    const distFolder = core.getInput("dist-folder", { required: true, trimWhitespace: true });
+
+    //  2. Upload files to S3
+    const s3URI = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`);
+
 }
 
 run();
